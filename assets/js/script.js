ae9a410cfb1290c9dfd27251c63413024dc787eb
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-05-27 14:00:10 -0400

    Modified code. index.html: modified form input...removed requirements from html style.css: updated styles for various elements script.js: added resetWeatherDisplay() to clear out the appended elements when a new city is searched.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
     var longitude;
     var currentForecast = $('#todays-weather');
     var futureForecast = $('#futurecast-parent');
+    var displayReset = false;
 
     function getLatandLong() {
         var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=" + APIKey;
@@ -38,57 +39,79 @@ $(document).ready(function(){
             })
             .then(function(data){
                 console.log(data);
-
-                var dateToday = moment.unix(data.current.dt).format("MM/DD/YYYY");
-                var cityName = $('<h2>').text(city + " (" + dateToday + ")");
-                var currentDescription = $('<p>').text("Description: " + data.current.weather[0].description);
-                var currentIcon = data.current.weather[0].icon;
-                var currentIconDisplay = $('<img>').attr('src',"http://openweathermap.org/img/w/" + currentIcon + ".png");
-                var currentTemp = $('<p>').text("Temp: " + data.current.temp + "\u00B0C");
-                var currentHumidity = $('<p>').text("Humidity: " + data.current.humidity + "%");
-                var currentWind = $('<p>').text("Wind Speed: " + data.current.wind_speed + " MPH");
-                var currentUvi = $('<p>').text("UV Index: " + data.current.uvi);
-                currentForecast.append(cityName);
-                cityName.append(currentIconDisplay)
-                currentForecast.append(currentDescription);
-                currentForecast.append(currentTemp);
-                currentForecast.append(currentHumidity);
-                currentForecast.append(currentWind);
-                currentForecast.append(currentUvi);
-
-                for (var i=1; i<6; i++) {
-
-                    var dateFuture = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
-                    var futureDateDisplay = $('<h4>').text(dateFuture);
-                    var futureIcon = data.daily[i].weather[0].icon;
-                    var futureIconDisplay = $('<img>').attr('src',"http://openweathermap.org/img/w/" + futureIcon + ".png");
-                    var futureTemp = $('<p>').text("Temp: " + data.daily[i].temp.day + "\u00B0C");
-                    var futureHumidity = $('<p>').text("Humidity: " + data.daily[i].humidity + "%");
-                    var futureWind = $('<p>').text("Wind Speed: " + data.daily[i].wind_speed + " MPH");
-                    var futureContainer = $('<div>');
-                    futureContainer.addClass("futurecast");
-                    futureForecast.append(futureContainer);
-                    futureContainer.append(futureDateDisplay);
-                    futureContainer.append(futureIconDisplay)
-                    futureContainer.append(futureTemp);
-                    futureContainer.append(futureHumidity);
-                    futureContainer.append(futureWind);
-                   
-                }
-
+                                               
+                populateCurrentWeather(data);
+                populate5DayForecast(data);
 
             })
     }
 
-    fetchBtn.on('click', function(){
+    function populateCurrentWeather(data) {
+        var dateToday = moment.unix(data.current.dt).format("MM/DD/YYYY");
+        var cityName = $('<h2>').text(city + " (" + dateToday + ")");
+        var currentDescription = $('<p>').text("Description: " + data.current.weather[0].description);
+        var currentIcon = data.current.weather[0].icon;
+        var currentIconDisplay = $('<img>').attr('src',"http://openweathermap.org/img/w/" + currentIcon + ".png");
+        var currentTemp = $('<p>').text("Temp: " + data.current.temp + "\u00B0C");
+        var currentHumidity = $('<p>').text("Humidity: " + data.current.humidity + "%");
+        var currentWind = $('<p>').text("Wind Speed: " + data.current.wind_speed + " MPH");
+        var currentUvi = $('<p>').text("UV Index: " + data.current.uvi);
+        currentForecast.append(cityName);
+        cityName.append(currentIconDisplay)
+        currentForecast.append(currentDescription);
+        currentForecast.append(currentTemp);
+        currentForecast.append(currentHumidity);
+        currentForecast.append(currentWind);
+        currentForecast.append(currentUvi);
+
+        displayReset=true;
+        console.log(displayReset);
+    }
+
+    function populate5DayForecast(data) {
+        for (var i=1; i<6; i++) {
+            var dateFuture = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+            var futureDateDisplay = $('<h4>').text(dateFuture);
+            var futureIcon = data.daily[i].weather[0].icon;
+            var futureIconDisplay = $('<img>').attr('src',"http://openweathermap.org/img/w/" + futureIcon + ".png");
+            futureIconDisplay.attr('width', '75px');
+            var futureTemp = $('<p>').text("Temp: " + data.daily[i].temp.day + "\u00B0C");
+            var futureHumidity = $('<p>').text("Humidity: " + data.daily[i].humidity + "%");
+            var futureWind = $('<p>').text("Wind Speed: " + data.daily[i].wind_speed + " MPH");
+            var futureContainer = $('<div>');
+            futureContainer.addClass("futurecast");
+            futureForecast.append(futureContainer);
+            futureContainer.append(futureDateDisplay);
+            futureContainer.append(futureIconDisplay)
+            futureContainer.append(futureTemp);
+            futureContainer.append(futureHumidity);
+            futureContainer.append(futureWind);
+        }
+    }
+
+    function resetWeatherDisplay() {
+        currentForecast.empty();
+        futureForecast.empty();
+    }
+
+    fetchBtn.on('click', function(event){
+        event.preventDefault();
+        
         city = cityInput.val();
         console.log(city);
 
-        getLatandLong();
-        addSearchedCity();
+        if (displayReset===true) {
+            resetWeatherDisplay();
+        }
 
-        cityInput.val('');
-        
+        if (city !== '') {
+            getLatandLong();
+            addSearchedCity();     
+
+            cityInput.val('');   
+        } else {
+            alert ('City is required for search. Please try again.')
+        }
     })
 
     function addSearchedCity() {
